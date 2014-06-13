3ba9d25b4e4c1489fc2a7f81d127a8b4272f5703
Max Lansing <lansing@gmail.com>
2014-06-13 00:13:15 -0700

    clean before build

--- bin/build.rb
+++ bin/build.rb
@@ -3,12 +3,15 @@
 # Dharma Overground theme build script
 # written by Max Lansing (lansing@gmail.com)
 
+require 'fileutils'
+
 PRODUCT_NAME = "dho-theme"
 
 BASE_DIR = File.dirname(__FILE__) + "/../"
 STAGING_DIR = File.expand_path(BASE_DIR + "staging")
 THEME_DIR = File.expand_path(BASE_DIR + "theme")
 BUILD_DIR = File.expand_path(BASE_DIR + "build")
+THEME_WAR = "#{STAGING_DIR}/pixelpress-theme-6.2.0.0.war"
 
 run_command = lambda do |c|
   puts c
@@ -22,8 +25,12 @@ zip_into = lambda do |dir, destination|
   Dir.chdir(starting_dir)
 end
 
+File.delete(THEME_WAR) if File.exist?(THEME_WAR)
+FileUtils.rm_r("#{BUILD_DIR}") if Dir.exist?(BUILD_DIR)
 
-zip_into.call(THEME_DIR, "#{STAGING_DIR}/pixelpress-theme-6.2.0.0.war")
+Dir.mkdir(BUILD_DIR)
+
+zip_into.call(THEME_DIR, THEME_WAR)
 
 zip_into.call(STAGING_DIR, "#{BUILD_DIR}/#{PRODUCT_NAME}.lpkg")
 
